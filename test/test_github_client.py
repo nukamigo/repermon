02c822c006ec173b5c790e78ab03e2f0c005eb3c
NunoFrRibeiro <nunofrribeiro@hotmail.com>
2025-12-11 00:34:58 +0000

    chore(github_client): Add more unit testing
    Addresses: #12
    
    Signed-off-by: NunoFrRibeiro <nunofrribeiro@hotmail.com>

--- test/test_github_client.py
+++ test/test_github_client.py
@@ -1,6 +1,8 @@
 import pytest
 from src.github_client import GitHubClientError, WorkflowRuns
 from test.data import test_github_client_data
+from unittest.mock import patch
+from requests.exceptions import RequestException
 
 
 @pytest.fixture
@@ -25,4 +27,22 @@ class TestWorkflowRun:
         with pytest.raises(GitHubClientError) as exc:
             workflow_client.get_last_workflow_runs()
 
-        assert "owner/name" in str(exc.value)
+        assert "Repository name must be in the format 'owner/name'." in str(exc.value)
+
+    def test_invalid_token(self, valid_repo):
+        workflow_client = WorkflowRuns(repo_name=valid_repo, token="")
+
+        with pytest.raises(GitHubClientError) as exc:
+            workflow_client.get_last_workflow_runs()
+
+        assert "GitHub API token must be provided." in str(exc.value)
+
+    @patch("src.github_client.requests.post")
+    def test_get_last_workflow_runs_request_exception(self, mock_post, workflow_client):
+        """Should raise GitHubClientError on network errors."""
+        mock_post.side_effect = RequestException("Network error")
+        with pytest.raises(GitHubClientError) as exc_info:
+            workflow_client.get_last_workflow_runs()
+
+        assert "Request error" in str(exc_info.value)
+        mock_post.assert_called_once()
